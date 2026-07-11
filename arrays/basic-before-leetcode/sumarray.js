a4c6540b1388ce3a160cab00cffe000c04762be8
uzair Mustafa <pssuzair@gmail.com>
2026-07-11 16:24:34 +0500

    sum using reduce

--- arrays/basic-before-leetcode/sumarray.js
+++ arrays/basic-before-leetcode/sumarray.js
@@ -1,6 +1,17 @@
-const arr=[1,2,3,4,5]
+let arr=[1,2,3,4,5]
 let sum=0
 for (num of arr){
     sum+=num
 }
 console.log(sum)
+
+
+//using reduce
+
+let arr1=[...arr]; //copying the element
+let sum1=arr1.reduce((acc,current)=>{
+    return acc+current
+},0)
+
+
+console.log(sum1)
