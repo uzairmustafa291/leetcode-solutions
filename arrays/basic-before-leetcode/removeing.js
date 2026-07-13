b052ccd62ec5a9dc8bf8a0b42c1e13e3cada50fa
uzair Mustafa <uzairmustafaofficial0@gmail.com>
2026-07-13 22:11:11 +0500

    removing element from array at any place

---
+++ arrays/basic-before-leetcode/removeing.js
@@ -0,0 +1,6 @@
+const arr=[1,2,3,4,5]
+arr.pop() // last element, 1,2,3,4
+arr.splice(3,1)  //element at third index 1,2,3,5 
+arr.shift() //first element, 2,3
+
+console.log(arr)
---
+++ arrays/mergeintervals_56.js
@@ -0,0 +1,14 @@
+var merge = function(intervals) {
+
+    intervals.sort((a,b)=>a[0]-b[0])
+    for(let i=0;i<intervals.length;i++){
+     if(intervals[i][1]>=intervals[i+1][0]){
+        if(intervals[i][1]<intervals[i+1][1]){
+            intervals[i][1]=intervals[i+1][1]
+            intervals.splice(i+1,1)
+        }
+     }
+
+    }
+    
+};
