83d3c407e87df0310149c7c6d8688bf5e37cf17e
uzair Mustafa <uzairmustafaofficial0@gmail.com>
2026-07-14 21:47:54 +0500

    stack basics added using c++

---
+++ stack/basic_beofore_leetcode.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+stack <int> s;
+s.push(1);
+s.push(4);
+cout<<s.top()<<endl;
+int s1=s.top();
+cout<<s1<<endl;
+return 0;
+}
+
+/* 
+important notes to be noted,
+no index wise getting elements 
+only play with top element 
+use only push pop and top whenever needed
+that is enough for stack.
+*/
