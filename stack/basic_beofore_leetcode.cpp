aef1b1c7bf0779e56e655a4621fd11b6b9f02732
uzair Mustafa <uzairmustafaofficial0@gmail.com>
2026-07-14 21:50:57 +0500

    empty is added to the stack

--- stack/basic_beofore_leetcode.cpp
+++ stack/basic_beofore_leetcode.cpp
@@ -9,6 +9,12 @@ s.push(4);
 cout<<s.top()<<endl;
 int s1=s.top();
 cout<<s1<<endl;
+if(s.empty()){
+    cout<<"stcck is empty"<<endl;
+}
+else {
+    cout<<"stack is not empty"<<endl;
+}
 return 0;
 }
 
